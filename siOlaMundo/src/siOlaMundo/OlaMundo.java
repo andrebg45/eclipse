90f46848de04202adfc010c3600c0ead31eebbac
1B Modular Info <1binfo@basilides.local>
2019-05-16 12:05:43 -0300

    novo teste

---
+++ siOlaMundo/src/siOlaMundo/OlaMundo.java
@@ -0,0 +1,16 @@
+package siOlaMundo;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		int v1,v2;
+		v1 = 10;
+		v2 = 15;
+		System.out.println("soma é " +(v1+v2));
+		System.out.println("subtração é " +(v1-v2));
+		System.out.println("multiplicação é "+(v1*v2));
+		System.out.println("divisao é "+(v1/v2));
+
+	}
+
+}
